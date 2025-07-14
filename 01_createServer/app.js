dd09dae3cf7bd638bf430b5daaa1fecb47933737
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-07-14 23:21:42 +0530

    create basic server

---
+++ 01_createServer/app.js
@@ -0,0 +1,23 @@
+// console.log("Hello, World!");
+
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    if(req.url == "/about"){
+        res.end("About Us Page");
+    }
+
+    if(req.url == "/contact"){
+        res.end("Contact Us Page");
+    }
+
+    if(req.url == "/services"){
+        res.end("Services Page");
+    }
+
+    if(req.url == "/"){
+        res.end("Home Page");
+    }
+})
+
+server.listen(3000)
