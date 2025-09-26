ee3233e1e3f088ef9254804920990b1d100315dc
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-09-26 22:08:56 +0530

    small changes

--- 04_expressgeneratorLearning/routes/users.js
+++ 04_expressgeneratorLearning/routes/users.js
@@ -4,16 +4,17 @@
 // make a schema
 // create model and export
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // mongoose.connect('mongodb://127.0.0.1:27017/dbname'); // 127.0.0.1 --> localhost
-mongoose.connect('mongodb://127.0.0.1:27017/testdbname'); // to create db if not exists
+mongoose.connect("mongodb://127.0.0.1:27017/testdbname"); // to create db if not exists
 
-const userschema = mongoose.Schema({ // schema is like blueprint of document
+const userschema = mongoose.Schema({
+  // schema is like blueprint of document
   username: String,
   name: String,
-  age: Number
-})
+  age: Number,
+});
 
 // mongoose.model(naam, schema); // model is like collection of documents
-module.exports = mongoose.model('users', userschema); // users is collection name
+module.exports = mongoose.model("users", userschema); // users is collection name
